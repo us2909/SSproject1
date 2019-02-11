91ed70c8618abf372d3eeca9a6274ac27d6be1ed
us2909 <us2909@users.noreply.github.com>
2019-02-11 18:43:40 +0530

    Added the code

---
+++ UltraCode_v1.ino
@@ -0,0 +1,42 @@
+#include <AFMotor.h>
+#include <Servo.h>
+#define TRIG_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 200 
+#define TURN_DIST COLL_DIST+10 
+
+AF_DCMotor motorright(1);
+AF_DCMotor motorleft(2);
+const int trigPin = A0;
+const int echoPin = A1;
+void setup() {
+  
+  //Serial.begin(9600); 
+motorright.setSpeed(255);
+motorleft.setSpeed(255);
+   pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  delay(10);
+ 
+if(distance<20)  
+ {
+  motorright.run(BACKWARD);             
+ } 
+ else if (distance>20)
+ {
+   motorright.run(BACKWARD);
+   motorleft.run(BACKWARD);
+ }
+ 
+}
